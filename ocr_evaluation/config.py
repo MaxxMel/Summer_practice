cae901172ce20da9a9fde0504a226de68b2d9c41
Maxim Melnikov <157640401+MaxxMel@users.noreply.github.com>
2025-07-23 17:26:24 +0300

    Update config.py

--- ocr_evaluation/config.py
+++ ocr_evaluation/config.py
@@ -6,4 +6,4 @@ DATA_DIR = os.path.join(BASE_DIR, "data")
 PROCESSED_DATA_DIR = os.path.join(DATA_DIR, "processed")
 
 # Имена датасетов
-DATASETS = ["NT", "d1", "d2", "d2_1", "d2_2", "d5", "d6"]
+DATASETS = ["Dataset1", "Dataset2", "dataset2_1", "dataset2_2", "Dataset5", "Dataset6"]
